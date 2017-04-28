2edde98fe4bbcc4d73e4a0b8627a2ee677f0fe3a
Jason Cope <jason.cope@noaa.gov>
2017-04-27 23:24:21 -0700

    Running for three species types

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 require(shiny)
 require(ggplot2)
+source('load_pops.r',local = FALSE)
 
 shinyServer(
   function(input, output) 
@@ -71,117 +72,141 @@ shinyServer(
       return(SBPRF)
     }
     
-    BHRecruit.max<- function(R0,steep,Weight,Maturity,M,F_yr,Sel,ages,a_i)
+    BHRecruit.max<- function(R0,Rt,steep,Weight,Maturity,M,F_yr,Sel,ages,a_i,sigmaR)
     {
       SBPRF0<-SpawnB(R0,Maturity,Weight,M,0,Sel,ages,a_i)
-      SBPRF<-SpawnB(R0,Maturity,Weight,M,F_yr,Sel,ages,a_i)
-      RF<-(R0*((4*steep*SBPRF)-(SBPRF0*(1-steep))))/(SBPRF*(5*steep-1))* exp(rnorm(1,0,0.5)-0.5^2/2)
+      SBPRF<-SpawnB(Rt,Maturity,Weight,M,F_yr,Sel,ages,a_i)
+      RF<-(R0*((4*steep*SBPRF)-(SBPRF0*(1-steep))))/(SBPRF*(5*steep-1))* exp(rnorm(1,0,sigmaR)-sigmaR^2/2)
       return(RF)
     }
     
-    
 ####### END FUNCTIONS ########
     
+    Pop.out.rockfish<- reactive({
+      species.in<-rockfish.in
+      ages=c(0:species.in$maxage)  
+      Sel= HCR.Log.Sel.fit(c(species.in$Sel.a,species.in$Sel.b),ages)
+      Lt.out<-HCR.VBGF(species.in$Linf,species.in$K,species.in$t0,ages)
+      Maturity<-Maturity<-HCR.Mat.fit(c(species.in$Mat.a,species.in$Mat.b),Lt.out)
+      Weight<-HCR.LtWt.fit(c(species.in$Ltwt.a,species.in$Ltwt.b),Lt.out)
+      
+      Num_all<-Num_F<-Num_M<-matrix(NA,length(ages),input$years.rockfish+1)
+      SB.ts<-Rec.ts<-rep(NA,input$years.rockfish+1)
+      
+      Num<-Numbers(R_in=species.in$R0,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Num_all[,1]<-rowSums(Num)
+      Num_F[,1]<-Num[,1]
+      Num_M[,2]<-Num[,2]
+      SB.ts[1]<-SpawnB(R_in=species.in$R0,Maturity,Weight,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Rec.ts[1]<-BHRecruit.max(R0=species.in$R0,Rt=species.in$R0,species.in$steep,Weight,Maturity,species.in$M,F_yr=0,Sel,ages,species.in$a_i,sigmaR=0)
+      for(i in 1:input$years.rockfish)
+      {
+        Num<-Numbers(R_in=Rec.ts[i],species.in$M,F_yr=input$F.in.rockfish,Sel,ages,species.in$a_i)
+        Num_all[,i+1]<-rowSums(Num)
+        Num_F[,i+1]<-Num[,1]
+        Num_M[,i+1]<-Num[,2]
+        SB.ts[i+1]<-SpawnB(R_in=Rec.ts[i],Maturity,Weight,species.in$M,F_yr=input$F.in.rockfish,Sel,ages,species.in$a_i)
+        Rec.ts[i+1]<-BHRecruit.max(R0=species.in$R0,Rt=Rec.ts[i],species.in$steep,Weight,Maturity,species.in$M,F_yr=input$F.in.rockfish,Sel,ages,species.in$a_i,sigmaR=0.25)
+      }
+      Pop.out.rockfish<-list(Num_all,Num_F,Num_M,SB.ts,Rec.ts)
+      names(Pop.out.rockfish)<-c("Numbers_all","Numbers_F","Numbers_M","SB","Rec")
+      return(Pop.out.rockfish)
+    })
     
- Pop_out<- reactive({
-   Lt.out<-HCR.VBGF(Linf,K,t0,0:maxage)
-   Maturity<-HCR.Mat.fit(c(-0.5,40),Lt.out)
-   Weight<-HCR.LtWt.fit(c(0.0000118,3.094),Lt.out)
-   
-   Num_all<-Num_F<-Num_M<-matrix(NA,length(ages),years+1)
-   SB.ts<-Rec.ts<-rep(NA,years+1)
-   
-   Num<-Numbers(R_in=R0,M,F_yr=0,Sel,ages,a_i)
-   Num_all[,1]<-rowSums(Num)
-   Num_F[,1]<-Num[,1]
-   Num_M[,2]<-Num[,2]
-   SB.ts[1]<-SpawnB(R_in=R0,Maturity,Weight,M,F_yr=0,Sel,ages,a_i)
-   Rec.ts[1]<-BHRecruit.max(R0,steep,Weight,Maturity,M,F_yr=0,Sel,ages,a_i)
-
-   for(i in 1:years)
-   {
-     Num<-Numbers(R_in=Rec.ts[i],M,F_yr=F.in,Sel,ages,a_i)
-     Num_all[,i+1]<-rowSums(Num)
-     Num_F[,i+1]<-Num[,1]
-     Num_M[,i+1]<-Num[,2]
-     SB.ts[i+1]<-SpawnB(R_in=Rec.ts[i],Maturity,Weight,M,F_yr=F.in,Sel,ages,a_i)
-     Rec.ts[i+1]<-BHRecruit.max(R0,steep,Weight,Maturity,M,F_yr=F.in,Sel,ages,a_i)
-   }
-    Pop.out<-list(Num_all,Num_F,Num_M,SB.ts,Rec.ts)
-    names(Pop.out)<-c("Numbers_all","Numbers_F","Numbers_M","SB","Rec")
-    Pop.out
-   })
      
- output$Mplot <- renderPlot({
-   M_vals_all<-M_vals_all()
-   M_methods<-c("Then_Amax 1","Then_Amax 2","Then_Amax 3","Hamel_Amax","AnC","Then_VBGF","Jensen_VBGF 1","Jensen_VBGF 2","Pauly_lt","Gislason","Chen-Wat","Roff","Jensen_Amat","Pauly_wt","PnW","Lorenzen","GSI")
-  # plot M
-   if(all(is.na(M_vals_all))){ymax<-0.5}
-   if(!(all(is.na(M_vals_all)))){ymax<-ceiling((max(M_vals_all,na.rm=TRUE)*1.1*10))/10}
-   par(mar=c(8,4,2,6),xpd =TRUE)
-   plot(M_vals_all, col = "black",bg=c("blue","blue","blue","blue","green","green","green","green","yellow","yellow","orange","red","red","black","black","black","purple"),xlab=" ",ylab="Natural mortality",ylim=c(0,ymax),pch=22,cex=1.5,axes=F)
-   box()
-   axis(1,at=1:length(M_vals_all),labels=M_methods,las=3)
-   axis(2)
-   legend(x="topright",legend=c("Amax","VBGF","VBGF:Temp","VBGF;Amat","Amat","Weight","GSI"),pch=22,col="black",pt.bg=c("blue","green","yellow","orange","red","black","purple"),bty="n",horiz=FALSE,cex=1,inset=c(-0.125,0))
-   M_table<-data.frame(cbind(M_methods,M_vals_all))
-   colnames(M_table)<-c("Method","M")
-  # if(all(is.na(M_vals()))){return(NULL)}
-   output$downloadMs <- downloadHandler(
-    filename = function() {paste0("M_values", '.csv') },
-    content = function(file) {write.csv(M_table, file=file)}
-  )
- })
+    Pop.out.flatfish<- reactive({
+      species.in<-flatfish.in
+      ages=c(0:species.in$maxage)  
+      Sel= HCR.Log.Sel.fit(c(species.in$Sel.a,species.in$Sel.b),ages)
+      Lt.out<-HCR.VBGF(species.in$Linf,species.in$K,species.in$t0,ages)
+      Maturity<-Maturity<-HCR.Mat.fit(c(species.in$Mat.a,species.in$Mat.b),Lt.out)
+      Weight<-HCR.LtWt.fit(c(species.in$Ltwt.a,species.in$Ltwt.b),Lt.out)
+      
+      Num_all<-Num_F<-Num_M<-matrix(NA,length(ages),input$years.flatfish+1)
+      SB.ts<-Rec.ts<-rep(NA,input$years.flatfish+1)
+      
+      Num<-Numbers(R_in=species.in$R0,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Num_all[,1]<-rowSums(Num)
+      Num_F[,1]<-Num[,1]
+      Num_M[,2]<-Num[,2]
+      SB.ts[1]<-SpawnB(R_in=species.in$R0,Maturity,Weight,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Rec.ts[1]<-BHRecruit.max(R0=species.in$R0,Rt=species.in$R0,species.in$steep,Weight,Maturity,species.in$M,F_yr=0,Sel,ages,species.in$a_i,sigmaR=0)
+      for(i in 1:input$years.flatfish)
+      {
+        Num<-Numbers(R_in=Rec.ts[i],species.in$M,F_yr=input$F.in.flatfish,Sel,ages,species.in$a_i)
+        Num_all[,i+1]<-rowSums(Num)
+        Num_F[,i+1]<-Num[,1]
+        Num_M[,i+1]<-Num[,2]
+        SB.ts[i+1]<-SpawnB(R_in=Rec.ts[i],Maturity,Weight,species.in$M,F_yr=input$F.in.flatfish,Sel,ages,species.in$a_i)
+        Rec.ts[i+1]<-BHRecruit.max(R0=species.in$R0,Rt=Rec.ts[i],species.in$steep,Weight,Maturity,species.in$M,F_yr=input$F.in.flatfish,Sel,ages,species.in$a_i,sigmaR=0.5)
+      }
+      Pop.out.flatfish<-list(Num_all,Num_F,Num_M,SB.ts,Rec.ts)
+      names(Pop.out.flatfish)<-c("Numbers_all","Numbers_F","Numbers_M","SB","Rec")
+      return(Pop.out.flatfish)
+    })
+    
+    Pop.out.shark<- reactive({
+      species.in<-shark.in
+      ages=c(0:species.in$maxage)  
+      Sel= HCR.Log.Sel.fit(c(species.in$Sel.a,species.in$Sel.b),ages)
+      Lt.out<-HCR.VBGF(species.in$Linf,species.in$K,species.in$t0,ages)
+      Maturity<-Maturity<-HCR.Mat.fit(c(species.in$Mat.a,species.in$Mat.b),Lt.out)
+      Weight<-HCR.LtWt.fit(c(species.in$Ltwt.a,species.in$Ltwt.b),Lt.out)
+      
+      Num_all<-Num_F<-Num_M<-matrix(NA,length(ages),input$years.shark+1)
+      SB.ts<-Rec.ts<-rep(NA,input$years.shark+1)
+      
+      Num<-Numbers(R_in=species.in$R0,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Num_all[,1]<-rowSums(Num)
+      Num_F[,1]<-Num[,1]
+      Num_M[,2]<-Num[,2]
+      SB.ts[1]<-SpawnB(R_in=species.in$R0,Maturity,Weight,species.in$M,F_yr=0,Sel,ages,species.in$a_i)
+      Rec.ts[1]<-BHRecruit.max(R0=species.in$R0,Rt=species.in$R0,species.in$steep,Weight,Maturity,species.in$M,F_yr=0,Sel,ages,species.in$a_i,sigmaR=0)
+      for(i in 1:input$years.shark)
+      {
+        Num<-Numbers(R_in=Rec.ts[i],species.in$M,F_yr=input$F.in.shark,Sel,ages,species.in$a_i)
+        Num_all[,i+1]<-rowSums(Num)
+        Num_F[,i+1]<-Num[,1]
+        Num_M[,i+1]<-Num[,2]
+        SB.ts[i+1]<-SpawnB(R_in=Rec.ts[i],Maturity,Weight,species.in$M,F_yr=input$F.in.shark,Sel,ages,species.in$a_i)
+        Rec.ts[i+1]<-BHRecruit.max(R0=species.in$R0,Rt=Rec.ts[i],species.in$steep,Weight,Maturity,species.in$M,F_yr=input$F.in.shark,Sel,ages,species.in$a_i,sigmaR=0.1)
+      }
+      Pop.out.shark<-list(Num_all,Num_F,Num_M,SB.ts,Rec.ts)
+      names(Pop.out.shark)<-c("Numbers_all","Numbers_F","Numbers_M","SB","Rec")
+      return(Pop.out.shark)
+    })
+    
+    
+       output$rockfish_pop <- renderPlot({
+       Pop.out.rockfish<-Pop.out.rockfish()
+       for(i in 1:length(Pop.out.rockfish$SB)){if(Pop.out.rockfish$SB[i]<0){Pop.out.rockfish$SB[i:length(Pop.out.rockfish$SB)]=0}}
+       SB.gg<-as.data.frame(cbind(c(1:(input$years.rockfish+1)),Pop.out.rockfish$SB))
+       names(SB.gg)<-c("Year","SB")
+       extict<-SB.gg$SB==0
+       rockfish_pop<-ggplot(data=SB.gg,aes(x=Year,y=SB))+geom_line(col="orange",lwd=1.5)+ylim(0, max(SB.gg$SB)*1.1)+geom_line(data=SB.gg[extict,],col="black",lwd=1.5)
+       print(rockfish_pop)
+     })
 
-
-#Plot Composite M
- output$Mcomposite<- renderPlot({    
-   if(all(is.na(M_vals_all()))){return(NULL)}
-   else{
-   M.wts<-c(input$Then_Amax_1,input$Then_Amax_2,input$Then_Amax_3,input$Hamel_Amax,input$AnC,input$Then_VBGF,input$Jensen_VBGF_1,input$Jensen_VBGF_2,input$Pauly_lt,input$Gislason,input$Chen_Wat,input$Roff,input$Jensen_Amat,input$Pauly_wt,input$PnW,input$Lorenzen,input$Gonosoma)
-   #remove NAs
-   if(any(is.na(M_vals_all()))){
-     NA.ind<-attributes(na.omit(M_vals_all()))$na.action
-     M.sub<-M_vals_all()[-NA.ind]
-     M.wts.sub<-M.wts[-NA.ind]
-   }
-   else{
-     M.sub<-M_vals_all()
-     M.wts.sub<-M.wts
-   }
-   #remove 0 weight
-   M.sub.n0<-M.sub[M.wts.sub>0]
-   M.wts.sub.n0<-M.wts.sub[M.wts.sub>0]
-   M.wts.sub.stand<-M.wts.sub.n0/sum(M.wts.sub.n0)
-   M.densum<-density(M.sub.n0,weights=M.wts.sub.stand,from=0,cut=0)
-   #Approximate the denisty function
-   f<- approxfun(M.densum$x, M.densum$y, yleft=0, yright=0)
-   #Standardize densities
-   pdf_counts<-round(100000*(M.densum$y/sum(M.densum$y)))
-   #Expand densities to samples
-   pdf.samples<-unlist(mapply(rep,M.densum$x,pdf_counts))
-   #Calculate the cdf
-   cdf.out<-ecdf(pdf.samples)
-   #Plot the density function
-   M.densum.plot<- data.frame(x = M.densum$x, y = M.densum$y)
-   Mcomposite.densityplot<- ggplot(data=M.densum.plot,aes(x,y,fill="blue"))+
-     geom_line(col="black")+
-     labs(x="Natural Mortality",y="Density")+ 
-     geom_area(fill="gray")+ 
-     #scale_x_continuous(limits=c(0,quantile(M.densum$x,0.99)))+
-     geom_vline(xintercept = quantile(cdf.out,0.5),color="darkblue",size=1.2)
-   print(Mcomposite.densityplot)
-   output$downloadMcompositedensityplot <- downloadHandler(
-   filename = function() { paste0('Mcomposite_densityplot',Sys.time(), '.png')},
-   content = function(file) {
-     png(file, type='cairo',width=800,height=720)
-     print(Mcomposite.densityplot)
-     dev.off()},contentType = 'image/png') 
-   output$downloadMcompositedist <- downloadHandler(
-     filename = function() {  paste0("Mcomposite_samples",Sys.time(),".DMP") },
-     content = function(file) {save(pdf.samples,file=file)}) 
-    }
-   })
+       output$flatfish_pop <- renderPlot({
+       Pop.out.flatfish<-Pop.out.flatfish()
+       for(i in 1:length(Pop.out.flatfish$SB)){if(Pop.out.flatfish$SB[i]<0){Pop.out.flatfish$SB[i:length(Pop.out.flatfish$SB)]=0}}
+       SB.gg<-as.data.frame(cbind(c(1:(input$years.flatfish+1)),Pop.out.flatfish$SB))
+       names(SB.gg)<-c("Year","SB")
+       extict<-SB.gg$SB==0
+       flatfish_pop<-ggplot(data=SB.gg,aes(x=Year,y=SB))+geom_line(col="green",lwd=1.5)+ylim(0, max(SB.gg$SB)*1.1)+geom_line(data=SB.gg[extict,],col="black",lwd=1.5)
+       print(flatfish_pop)
+       })
+     
+     output$shark_pop <- renderPlot({
+       Pop.out.shark<-Pop.out.shark()
+       for(i in 1:length(Pop.out.shark$SB)){if(Pop.out.shark$SB[i]<0){Pop.out.shark$SB[i:length(Pop.out.shark$SB)]=0}}
+       SB.gg<-as.data.frame(cbind(c(1:(input$years.shark+1)),Pop.out.shark$SB))
+       names(SB.gg)<-c("Year","SB")
+       extict<-SB.gg$SB==0
+       shark_pop<-ggplot(data=SB.gg,aes(x=Year,y=SB))+geom_line(col="blue",lwd=1.5)+ylim(0, max(SB.gg$SB)*1.1)+geom_line(data=SB.gg[extict,],col="black",lwd=1.5)
+       print(shark_pop)
+     })
+     
   }
 )
 
---
+++ species_types.R
@@ -0,0 +1,52 @@
+rockfish.in<-list(maxage=65,
+                  Linf=61,
+                  K=0.13,
+                  t0=0,
+                  R0=1000,
+                  M = 0.05,
+                  a_i=1,
+                  steep=0.7,
+                  Ltwt.a=0.0000118,
+                  Ltwt.b=3.094,
+                  Mat.a=-0.5,
+                  Mat.b=40,
+                  Sel.a=10,
+                  Sel.b=0.5
+                  )
+
+
+flatfish.in<-list(maxage=65,
+                  Linf=61,
+                  K=0.2,
+                  t0=0,
+                  R0=1000,
+                  M = 0.15,
+                  a_i=1,
+                  steep=0.82,
+                  Ltwt.a=0.0000305,
+                  Ltwt.b=3.36,
+                  Mat.a=-0.5,
+                  Mat.b=33,
+                  Sel.a=5,
+                  Sel.b=0.5
+                  )
+
+shark.in<-list(maxage=95,
+                  Linf=109,
+                  K=0.0262574,
+                  t0=0,
+                  R0=1000,
+                  M = 0.064,
+                  a_i=1,
+                  steep=0.82,
+                  Ltwt.a=0.00000231,
+                  Ltwt.b=3.15,
+                  Mat.a=-0.5,
+                  Mat.b=88.2,
+                  Sel.a=50,
+                  Sel.b=0.5)
+
+save(rockfish.in,file="C:/Users/Jason.Cope/Documents/GitHub/Pop_Model_4_Kids_Days/rockfish.DMP")
+save(flatfish.in,file="C:/Users/Jason.Cope/Documents/GitHub/Pop_Model_4_Kids_Days/flatfish.DMP")
+save(shark.in,file="C:/Users/Jason.Cope/Documents/GitHub/Pop_Model_4_Kids_Days/shark.DMP")
+
